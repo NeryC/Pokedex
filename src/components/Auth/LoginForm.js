d80883749c4a4d9d566112c4caf3b31deac5aac0
NeryC <nalbertoc132@gmail.com>
2024-03-22 21:46:48 -0300

    mejorando el diseño del login

--- src/components/Auth/LoginForm.js
+++ src/components/Auth/LoginForm.js
@@ -1,10 +1,41 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 import React from "react";
+import { TextInput } from "react-native-gesture-handler";
 
 export default function LoginForm() {
   return (
     <View>
-      <Text>LoginForm</Text>
+      <Text style={styles.title}>Iniciar Sesión</Text>
+      <TextInput
+        placeholder="Nombre de usuario"
+        style={styles.input}
+        au
+        toCapitalize="none"
+      />
+      <TextInput
+        placeholder="Contraseña"
+        style={styles.input}
+        autoCapitalize="none"
+        secureTextEntry={true}
+      />
+      <Button title="Entrar" onPress={() => console.log("entrando")} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  title: {
+    textAlign: "center",
+    fontSize: 28,
+    fontWeight: "bold",
+    marginTop: 50,
+    marginBottom: 15,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10,
+  },
+});
